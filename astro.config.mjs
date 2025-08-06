4a7c8ca41fce97602dff1e19d3cfd83e823fe495
fiko6306 <gaeulhome2@naver.com>
2025-08-06 15:03:16 +0900

    Add files via upload

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
+import icon from "astro-icon";
+import react from "@astrojs/react";
+
+export default defineConfig({
+  integrations: [tailwind(), icon(), react()],
+});
